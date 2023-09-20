fad1d7da391389b3c441daa9ff1545e4be277dd0
PiyushYadav88 <110554128+PiyushYadav88@users.noreply.github.com>
2023-09-20 23:46:39 +0530

    add

---
+++ app/src/main/java/com/ecommerce/project/ciphercart/fragments/shopping/CheckoutFragment.kt
@@ -0,0 +1,25 @@
+package com.ecommerce.project.ciphercart.fragments.shopping
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ecommerce.project.ciphercart.R
+
+// TODO: Rename parameter arguments, choose names that match
+
+class CheckoutFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_checkout, container, false)
+    }
+
+
+
+    }
