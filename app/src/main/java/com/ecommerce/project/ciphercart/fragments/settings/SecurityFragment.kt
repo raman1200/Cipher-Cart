14c7908b1650338703c326cab5f8cece423a0eb0
PiyushYadav88 <110554128+PiyushYadav88@users.noreply.github.com>
2023-09-20 20:44:36 +0530

    add

--- app/src/main/java/com/ecommerce/project/ciphercart/fragments/settings/SecurityFragment.kt
+++ app/src/main/java/com/ecommerce/project/ciphercart/fragments/settings/SecurityFragment.kt
@@ -7,14 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.ecommerce.project.ciphercart.R
+import com.ecommerce.project.ciphercart.databinding.FragmentProfileBinding
+import com.ecommerce.project.ciphercart.databinding.FragmentSecurityBinding
+
 class SecurityFragment : Fragment() {
 
+    private lateinit var binding: FragmentSecurityBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_security, container, false)
+
+
+        binding = FragmentSecurityBinding.inflate(layoutInflater, container, false)
+
+        clickListeners()
+
+        return binding.root
     }
 
+    private fun clickListeners() {
+        binding.apply {
+            ChangePassword.setOnClickListener {
+                findNavController().navigate(R.id.action_securityFragment_to_createNewPasswordFragment)
+            }
+            ChangePin.setOnClickListener {
+                findNavController().navigate(R.id.action_securityFragment_to_pinVerifyFragment2)
+            }
+
+        }
+    }
 }
