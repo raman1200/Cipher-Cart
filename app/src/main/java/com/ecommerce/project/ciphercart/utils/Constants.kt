033ad991b885354357072bd527bd984cf7e6e67c
RamanVerma-1 <rv18715@gmail.com>
2023-09-22 22:32:20 +0530

    update

--- app/src/main/java/com/ecommerce/project/ciphercart/utils/Constants.kt
+++ app/src/main/java/com/ecommerce/project/ciphercart/utils/Constants.kt
@@ -1,43 +1,11 @@
 package com.ecommerce.project.ciphercart.utils
 
-import android.app.Activity
-import android.content.Context
-import android.content.res.Resources
-import android.view.View
-import android.widget.Button
-import android.widget.Toolbar
-import androidx.fragment.app.Fragment
-import com.ecommerce.project.ciphercart.R
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
-
-fun etHintTextChange(editText: TextInputEditText, textInputLayout:TextInputLayout, hint:String) {
-    editText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-        if (hasFocus) {
-            textInputLayout.hint = "" // Set an empty string to remove the label
-        } else {
-            textInputLayout.hint = hint // Restore the label when not focused
-        }
+class Constants {
+    companion object {
+        const val USERS_COLLECTION = "users"
+        const val ADDRESS_COLLECTION = "addresses"
+        const val PRODUCTS_COLLECTION = "products"
+        const val CATEGORIES_COLLECTION = "categories"
+        const val CART_COLLECTION = "cart"
     }
-}
-fun setUpActionBar(toolbar: Toolbar, requireActivity:Activity) {
-    requireActivity.setActionBar(toolbar)
-    requireActivity.actionBar!!.setDisplayHomeAsUpEnabled(true)
-    requireActivity.actionBar!!.setDisplayShowTitleEnabled(false)
-    toolbar.setNavigationIcon(R.drawable.back_arrow)
-    toolbar.setNavigationOnClickListener {
-        requireActivity.onBackPressed()
-
-    }
-
-}
-
-fun disableButton(context: Context, btn:Button){
-    btn.isEnabled = false
-    btn.backgroundTintList = context.resources.getColorStateList(R.color.grey_600, null)
-}
-
-fun enableButton(context: Context, btn:Button){
-    btn.isEnabled = true
-    btn.backgroundTintList = context.resources.getColorStateList(R.color.black, null)
 }
---
+++ app/src/main/java/com/ecommerce/project/ciphercart/utils/Helper.kt
@@ -0,0 +1,41 @@
+package com.ecommerce.project.ciphercart.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.widget.Button
+import android.widget.Toolbar
+import com.ecommerce.project.ciphercart.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+fun etHintTextChange(editText: TextInputEditText, textInputLayout:TextInputLayout, hint:String) {
+    editText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+        if (hasFocus) {
+            textInputLayout.hint = "" // Set an empty string to remove the label
+        } else {
+            textInputLayout.hint = hint // Restore the label when not focused
+        }
+    }
+}
+fun setUpActionBar(toolbar: Toolbar, requireActivity:Activity) {
+    requireActivity.setActionBar(toolbar)
+    requireActivity.actionBar!!.setDisplayHomeAsUpEnabled(true)
+    requireActivity.actionBar!!.setDisplayShowTitleEnabled(false)
+    toolbar.setNavigationIcon(R.drawable.back_arrow)
+    toolbar.setNavigationOnClickListener {
+        requireActivity.onBackPressed()
+
+    }
+
+}
+
+fun disableButton(context: Context, btn:Button){
+    btn.isEnabled = false
+    btn.backgroundTintList = context.resources.getColorStateList(R.color.grey_600, null)
+}
+
+fun enableButton(context: Context, btn:Button){
+    btn.isEnabled = true
+    btn.backgroundTintList = context.resources.getColorStateList(R.color.black, null)
+}
